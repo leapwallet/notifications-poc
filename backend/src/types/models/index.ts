be811814aefcc6f92f0457f9f6b06a7f9e195e43
neelkamath <neelkamathonline@gmail.com>
2022-08-17 20:13:04 +0530

    Update dependencies

---
+++ backend/src/types/models/index.ts
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Apache-2.0
+
+// Auto-generated , DO NOT EDIT
+
+export {Transfer} from "./Transfer"
+
+export {Block} from "./Block"
+
+export {TransferEvent} from "./TransferEvent"
+
+export {Transaction} from "./Transaction"
+
+export {Message} from "./Message"
+
